06b3f2f96eab8497a50e003e09fd290252adf078
Atsushi Tadokoro <tadokoro@gmail.com>
2016-02-13 14:51:04 +0900

    add use vertex example

---
+++ example-usevertex/src/ofApp.cpp
@@ -0,0 +1,80 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+    ofBackground(0);
+    
+    video.initGrabber(1280, 720);
+    
+    isf.setup(1280, 720, GL_RGB32F);
+    isf.load("Circular Screen");
+    isf.setImage("inputImage", video.getTexture());
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    video.update();
+    isf.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    isf.draw(0, 0);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    isf.setUniform<ofVec2f>("center", ofPoint(x, y));
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
